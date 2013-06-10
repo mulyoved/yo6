a6d398ea4a5fa0066b713f1972f787188efed438
mulyoved <mulyoved@gmail.com>
2013-06-10 17:45:57 +0300

    deploy4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  */
 
 var server = require('./app_create');
-var port = process.env.VCAP_APP_PORT || 9000;
+var port = process.env.PORT || 5000;
 
 server.listen(port, function(){
   console.log('Express server listening on port ' + port);
